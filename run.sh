9379833bcf979ec36f4906b0731d1e644ab344a2
Michael Orr <michael@cloudspace.com>
2014-04-17 19:33:58 -0400

    bash logic cleanup

--- run.sh
+++ run.sh
@@ -3,35 +3,25 @@ if [ ! -f /.mysql_admin_created ]; then
   /create_mysql_admin_user.sh
 fi
 
-if [ "$setup" == "master" ]; then
-  if [ ! -f /.master_configured ]; then
-    /master_configure.sh
-  fi
+if [ ("$setup" == "master") -a (! -f /.master_configured) ]; then
+  /master_configure.sh
 fi
 
-if [ "$setup" == "slave" ]; then
+if [ "$setup" == "slave" -a (! -f /.slave_configured) ]; then
   echo "$MASTERDB_PORT_3306_TCP_ADDR - linked master db addr"
-  if [ ! -f /.slave_configured ]; then
-    /slave_configure.sh
-  fi
+  /slave_configure.sh
 fi
 
-if ["$dbname" != "" && "$setup" != "slave"]
-  if [ ! -f /.db_created ]; then
-    /create_db.sh $dbname
-  fi
+if [ ("$dbname" != "") -a (! -f /.db_created) ]; then
+  /create_db.sh $dbname
 fi
 
-if [ "$setup" == "master" ]; then
-  if [ ! -f /.slave_permission_granted ]; then
-    /grant_slave_permission.sh $slaving_username $slaving_password
-  fi
+if [ ("$setup" == "master") -a (! -f /.slave_permission_granted) ]; then
+  /grant_slave_permission.sh $slaving_username $slaving_password
 fi
 
-if [ "$setup" == "slave" ]; then
-  if [ ! -f /.slaving_started ]; then
-    /start_slaving.sh $slaving_username $slaving_password $bin_file $bin_position
-  fi
+if [ "$setup" == "slave" -a (! -f /.slaving_started) ]; then
+  /start_slaving.sh $slaving_username $slaving_password $bin_file $bin_position
 fi
 
 exec supervisord -n
